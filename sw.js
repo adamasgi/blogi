35e9a8a7c531b454f33903cc4c9cf436d6171771
AdamAsgi <git@adamasgi.com>
2022-11-03 23:12:13 -0700

    Updated service worker to only serve really static files from cache

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v8");
+const addResourcesToCache = async (resources, cacheName) => {
+  const cache = await caches.open(cacheName);
   await cache.addAll(resources);
 };
+
+const staticResources = [
+  	  "/favicon.ico",
+	  "/images/android-chrome-192x192.png",
+	  "/images/android-chrome-512x512.png",
+	  "/fonts/Inter-VariableFont_slnt,wght.ttf",
+	  "/fonts/ShareTechMono-Regular.ttf"
+]
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -9,16 +17,28 @@ self.addEventListener("install", (event) => {
       "/index.html",
       "/sw.js",
 	  "/css/index.css",
-	  "/favicon.ico",
 	  "/site.webmanifest",
-	  "/images/android-chrome-192x192.png",
-	  "/images/android-chrome-512x512.png",
-	  "/fonts/Inter-VariableFont_slnt,wght.ttf",
-	  "/fonts/ShareTechMono-Regular.ttf"
-    ])
+    ],"v9")
   );
+  event.waitUntil(
+    addResourcesToCache(staticResources, "static")
+  );
+  console.log("Hi there")
 });
 
+function inStatics(item, staticResources) {
+  for (const s of staticResources) {
+	if (item.includes(s)) {
+	  return true;
+	}}
+  return false;
+}
+
 self.addEventListener('fetch', function(event) {
-  console.log("fetch for :", event.request.url)
+  if (inStatics(event.request.url, staticResources)) {
+	event.respondWith(caches.match(event.request));
+	console.log(event.request.url, "url cached");
+  } else {
+	console.log(event.request.url, "url not matched");
+  }
 })
