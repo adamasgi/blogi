907d0ff8f1918de770a103c9552fa4ca3aa799b8
AdamAsgi <git@adamasgi.com>
2022-11-03 23:19:12 -0700

    Updated service worker to only serve really static files from cache

---
+++ index.js
@@ -0,0 +1,32 @@
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+	try {
+	  const registration = await navigator.serviceWorker.register("/sw.js", {
+		scope: "/",
+	  });
+	  if (registration.installing) {
+		console.log("Service worker installing");
+	  } else if (registration.waiting) {
+		console.log("Service worker installed");
+	  } else if (registration.active) {
+		console.log("Service worker active");
+	  }
+	} catch (error) {
+	  console.error(`Registration failed with ${error}`);
+	}
+  }
+};
+
+function isInstalled() {
+  // For iOS
+  if(window.navigator.standalone) return true
+
+  // For Android
+  if(window.matchMedia('(display-mode: standalone)').matches) return true
+
+  // If neither is true, it's not installed
+  return false
+}
+
+
+registerServiceWorker();
